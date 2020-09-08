69d611f9b1e82938da40372f8a90edfc15b4789d
stb <stanb0997@gmail.com>
2020-09-08 19:02:49 +0300

    solved

---
+++ 05.py
@@ -0,0 +1,12 @@
+# http://www.pythonchallenge.com/pc/def/peak.html
+
+import pickle
+import requests
+
+url = 'http://www.pythonchallenge.com/pc/def/banner.p'
+response = requests.get(url).content
+data = pickle.loads(response)
+
+for pair in data:
+    print(pair)
+    print(''.join([i * j for i, j in pair]))
